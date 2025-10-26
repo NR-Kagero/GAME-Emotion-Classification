dc6d4cf1ac00d29d084020ef9f529b23be3dc511
Kagero <20190393@stud.fci-cu.edu.eg>
2025-10-26 13:26:50 +0300

    reading and reshaping the Raw data

---
+++ main.py
@@ -0,0 +1,6 @@
+from Preprocessing import read_plot,read_all_files_paths
+
+allFiles=[]
+read_all_files_paths("C:\\Users\\Kagero\\PycharmProjects\\GAME-Emotion-Classification\\GAMEEMO",allFiles)
+print(allFiles)
+print(len(allFiles))
